bd312464715d2350f00f947733873fb56958f9c3
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-05-20 21:56:56 -0300

    Creating a reusable Checkbox component

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# taskade-mobile
+## Description
+
+[Backend](https://github.com/bugasmarcondes/taskade-backend), check it in order to have access to the GraphQL Api that is used throughout this course.
+
+## Troubleshoot
+
+- Had to install NVM with an older Node version in order to use Expo.
+- Had to install XCODE from Apple Store in order to execute the iPhone simulator.
+
+## Tutorial
+
+```bash
+expo init taskade-mobile
+# choose template = tabs (typescript)
+```
+
+```bash
+npm run start
+```
+
+Set of [icons](http://icons.expo.fyi) that can be used with Expo
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,27 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import Checkbox from "../components/Checkbox";
+import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
+  const [value, setValue] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Checkbox isChecked={value} onPress={() => setValue(!value)} />
+        <TextInput
+          style={{
+            flex: 1,
+            fontSize: 18,
+            color: "white",
+            marginLeft: 12,
+          }}
+          multiline
+        />
+      </View>
     </View>
   );
 }
@@ -17,16 +29,11 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    padding: 12,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    fontWeight: "bold",
   },
 });
